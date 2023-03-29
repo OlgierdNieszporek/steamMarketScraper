f01d324340178e517edac5dced60dde0667049db
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-03-29 23:41:04 +0200

    Refactor zmiennych i metod

--- scrapePage.py
+++ scrapePage.py
@@ -4,36 +4,27 @@ from bs4 import BeautifulSoup
 import requests
 import strings
 
-
-def scrapeSteamMarket():
-    iterateThroughWholeMarket()
-
-def createCasesList(casesnames, casesList):
-    for casename in casesnames:
-        casesList.append(casename.text)
-    return casesList
+def createProductsList(products_names, products_list):
+    for case_name in products_names:
+        products_list.append(case_name.text)
+    return products_list
 
 
-def createPricesList(prices, pricesList):
+def createPricesList(prices, prices_list):
     for price in prices:
-        pricesList.append(price.text)
-    return pricesList
+        prices_list.append(price.text)
+    return prices_list
 
 
-# def createProductsAndPricesMap(casesnames, prices):
-#     caseWithPricesMap = {case: price for case, price in zip(createCasesList(casesnames), createPricesList(prices))}
-#     return caseWithPricesMap
-
-
-def iterateThroughWholeMarket():
+def scrapeStockMarket():
     product_list, price_list = [], []
     url = strings.url
     page_to_scrape = requests.get(url)
     soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-    product_names = soup.findAll("td", attrs={"class": "colWalor textNowrap"})
+    product_names = soup.findAll("td", attrs={"class": strings.product_name_string})
+    prices = soup.findAll("td", attrs={"class": strings.prices_string})
     product_list = [name.text.strip() for name in product_names]
-    prices = soup.findAll("td", attrs={"class": "colKurs"})
     price_list = createPricesList(prices, price_list)
-    print(url)
     print(product_list)
     print(price_list)
+    return product_list, price_list
