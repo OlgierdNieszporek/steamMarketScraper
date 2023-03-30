ae8d4292a6cf9f151c0030ff4a8695ed7ad81e61
Lukasz Maciejewski <lks.maciejewski63@gmail.com>
2023-03-30 16:51:36 +0200

    Stworzyłem słownik i funkcję do printowania go klucz + wartość pod sobą, usunąłem zbędne metody i zmienne

--- scrapePage.py
+++ scrapePage.py
@@ -8,32 +8,30 @@ import strings
 def scrapeSteamMarket():
     iterateThroughWholeMarket()
 
-def createCasesList(casesnames, casesList):
-    for casename in casesnames:
-        casesList.append(casename.text)
-    return casesList
+
+def createDict(names, prices):
+    dictionary = {}
+    for i in range(0, len(names)):
+        dictionary[names[i]] = prices[i]
+    return dictionary
 
 
-def createPricesList(prices, pricesList):
-    for price in prices:
-        pricesList.append(price.text)
-    return pricesList
-
-
-# def createProductsAndPricesMap(casesnames, prices):
-#     caseWithPricesMap = {case: price for case, price in zip(createCasesList(casesnames), createPricesList(prices))}
-#     return caseWithPricesMap
+def printDict(dict):
+    for key, value in dict.items():
+        print(key, ': ', value)
 
 
 def iterateThroughWholeMarket():
-    product_list, price_list = [], []
     url = strings.url
     page_to_scrape = requests.get(url)
     soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+
     product_names = soup.findAll("td", attrs={"class": "colWalor textNowrap"})
     product_list = [name.text.strip() for name in product_names]
+
     prices = soup.findAll("td", attrs={"class": "colKurs"})
-    price_list = createPricesList(prices, price_list)
-    print(url)
-    print(product_list)
-    print(price_list)
+    prices_list = [price.text.strip() for price in prices]
+
+    dictionary = createDict(product_list, prices_list)
+
+    printDict(dictionary)
