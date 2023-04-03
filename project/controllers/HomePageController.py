9c371f6d20f7eca81b71d02fbb448ea17115e624
fmystek <fmystek@obrum.pl>
2023-04-03 14:25:22 +0200

    Poprawki w tablicy - dodanie dynamicznzie tworzonej tablicy z linakmi do podstron

--- project/controllers/HomePageController.py
+++ project/controllers/HomePageController.py
@@ -2,39 +2,27 @@
 import os
 import pandas as pd
 from project import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
+
+id = [0, 1]
+names = ['basic', 'test']
+values = [12, 15.223]
 
 #route index
 @app.route('/')
 def index():
-    data = {
-        "title": "KUPSKO",
-        "body": "KUPA KUPA KUPA"
-    }
-    return render_template('HomePage.html.j2', data = data)
+    df = pd.DataFrame({
+        'Id': [],
+        'Name':[],
+        'Value': []})
+
+    for i in range(0, len(id)):
+        df.loc[id[i]] = [id[i], names[i], values[i]]
+
+    df['Name'] = df['Name'].apply(lambda x: f'<a href="../{x}">{x}</a>')
+    table = df.to_html(index=False, escape=False)
+
+    return render_template("HomePage.html.j2", table=table)
 
 
 
-@app.route('/')
-@app.route('/Table')
-def table():
-    data_dic = {
-        'id': [100, 101, 102],
-        'color': ['red', 'blue', 'red']}
-    columns = ['id', 'color']
-    index = ['a', 'b', 'c']
-
-    df = pd.DataFrame(data_dic, columns=columns, index=index)
-    table2 = df.to_html(index=False)
-
-    return render_template("ProductPage.html.j2", table=table2)
-
-
-
-CHART_FOLDER = os.path.join('static', 'productCharts')
-app.config['CHART_FOLDER'] = CHART_FOLDER
-@app.route('/')
-@app.route('/image')
-def show_index():
-    full_filename = os.path.join(app.config['CHART_FOLDER'], 'Wykres.png')
-    return render_template("ImagePage.html.j2", user_image = full_filename)
--- project/controllers/ProductPageController.py
+++ project/controllers/ProductPageController.py
@@ -1,40 +1,19 @@
 import os
 
-from matplotlib.backends.backend_template import FigureCanvas
-from matplotlib.figure import Figure
-from project import app
-import io
-import base64
+import datetime
 import pandas as pd
 from project import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 
 
-from flask import Flask, send_file
 
-####
-#Do OGARNIECIA JAK W FLASKU WYSWEITLIC PNG
-###
-# przygotowanie danych
-
-
-#@app.get('/aaaImage')
-def show_image():
-    x = [1, 2, 3, 4, 5]
-    y = [1, 4, 9, 16, 25]
-    # wykres
-    fig = Figure()
-    ax = fig.subplots()
-    ax.plot(x, y)
-    # zapis do pliku png
-    png_output = io.BytesIO()
-    FigureCanvas(fig).print_png(png_output)
-    # kodowanie do base64
-    png_output = base64.b64encode(png_output.getvalue()).decode('utf-8')
-
-    # wyświetlenie w HTML
-    html = f'<img src="data:image/png;base64,{png_output}">'
-    png_output.save(os.path.join(app.root_path, 'static', 'customlogos', 'wykres.png'))
-    filename = os.path.join(app.root_path, 'static', 'customlogos', 'wykres.png')
-    # send_file(filename, mimetype='image/png')
-    return html
+CHART_FOLDER = os.path.join('static', 'productCharts')
+app.config['CHART_FOLDER'] = CHART_FOLDER
+@app.route('/')
+@app.route('/<product_name>')
+def show_index(product_name):
+    print(product_name)
+    full_filename = os.path.join(app.config['CHART_FOLDER'], 'Wykres_'+product_name+'.png')
+    print(full_filename)
+    #
+    return render_template("ProductPage.html.j2",  product_chart_image=full_filename, product_name=product_name,utc_dt=datetime.datetime.utcnow())
