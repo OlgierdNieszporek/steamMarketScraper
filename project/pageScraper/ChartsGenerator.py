1cf235aadedfa4c66717398c73cb4ecb3bd3a7a9
fmystek <fmystek@obrum.pl>
2023-04-03 16:26:51 +0200

    zmiana nazw osi w funkcji generatora wykresow

--- project/pageScraper/ChartsGenerator.py
+++ project/pageScraper/ChartsGenerator.py
@@ -6,13 +6,13 @@ from project import app
 
 
 
-def generateChart(product_name, xAxis , yAxis):
+def generateChart(product_name, xAxisData , yAxisData, xAxisName = 'x', yAxisName = 'y'):
     # mock danych
-    xAxis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    yAxis = [5, 7, 8, 1, 4, 9, 6, 3, 5, 2, 1, 8]
+    xAxisData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    yAxisData = [5, 7, 8, 1, 4, 9, 6, 3, 5, 2, 1, 8]
 
-    x_axis =xAxis
-    y_axis = yAxis
+    x_axis =xAxisData
+    y_axis = yAxisData
 
     plt.plot(x_axis, y_axis)
     plt.title(product_name)
