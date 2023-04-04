85a6358a830ddcb40a7d9d45228eabab20cd4b7a
Mosshart <filip.mystek@gmail.com>
2023-04-04 11:40:21 +0200

    Dodanie bazy (na stale) do repo, zmiana connection stringa

--- project/Database/establishConnection.py
+++ project/Database/establishConnection.py
@@ -1,6 +1,9 @@
+import os
 import sqlite3
 
+dbPath = os.path.join('project', 'Database', 'Chinook.db')
 
 def connect():
-    connection = sqlite3.connect("C:/Users/lksma/AppData/Roaming/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db")
+    print(dbPath)
+    connection = sqlite3.connect(dbPath)
     return connection
