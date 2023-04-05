fb6dcdfc3c58d9f1226bf6f72e5748edc0a6b51b
fmystek <fmystek@obrum.pl>
2023-04-05 16:27:16 +0200

    dosylam pliki

---
+++ project/models/ProductModel.py
@@ -0,0 +1,9 @@
+class ProductModel:
+    def __init__(self, id, productName, productPrice, date):
+        self.id = id
+        self.productName = productName
+        self.productPrice = productPrice
+        self.date = date
+
+    def __str__(self):
+        return f'Product:{self.id} {self.productName} {self.productPrice} {self.last_name} {self.date}'
