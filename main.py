8ff35994d301769d110ddea887aabd053034362b
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-20 18:46:24 +0200

    proba 2010230123feaeffeefsaef

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import threading
 from project import app
 from project.Database.databaseOperations import loadScrapeToDatabase
 # from project.pageScraper.ChartsGenerator import generateChart
-from project.pageScraper.multiThreading import *
 
 
 if __name__ == '__main__':
-    #runApp()
-    app.run(host='0.0.0.0', debug=True, use_reloader=False, threaded=True)
-    # app.run(host="localhost", port=8000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
