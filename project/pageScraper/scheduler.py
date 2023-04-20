deb4f870386cd2d94d2946457b839eca183a38ce
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-20 22:58:06 +0200

    proba 2010230123feaeffeefsaef

--- project/pageScraper/scheduler.py
+++ project/pageScraper/scheduler.py
@@ -6,22 +6,25 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from project.Database.databaseOperations import *
+iterator = 0
 
 def job():
-    #screapeData
-    #loadScrapeToDatabase()
+        #screapeData
+        #loadScrapeToDatabase()
 
-    #deleteFromDBIfOlderThanTwoWeeks
-    #removeDataFromDatabase()
+        #deleteFromDBIfOlderThanTwoWeeks
+        #removeDataFromDatabase()
 
-    #generateNewCharts
+        #generateNewCharts
 
-    #updateFront
-    print("Done scheduled tasks")
+        #updateFront
+        print("Done scheduled tasks")
+
 
 
 def runScheduler():
     sched = BackgroundScheduler(daemon=False)
     sched.add_job(job, 'interval', seconds=5)
+    print("dupacycejakdonice")
     sched.start()
     atexit.register(lambda: sched.shutdown())
