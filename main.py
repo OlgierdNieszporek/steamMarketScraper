9b82290d70d9b75f4bf4f3b9c549525ac286ad70
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-20 17:11:41 +0200

    proba 2010230123feaeffeefsaef

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from project.Database.databaseOperations import loadScrapeToDatabase
 # from project.pageScraper.ChartsGenerator import generateChart
 from project.pageScraper.multiThreading import *
 
+def appRunner():
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
 if __name__ == '__main__':
-    runApp()
+    #runApp()
+    appRunner()
     # app.run(host="localhost", port=8000, debug=True, use_reloader=False)
