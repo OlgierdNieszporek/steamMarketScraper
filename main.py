b0c83c8eded39b8b821e69bbf45d97775686a489
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-20 19:12:52 +0200

    proba 2010230123feaeffeefsaef

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from project.Database.databaseOperations import loadScrapeToDatabase
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', debug=False, use_reloader=False)
