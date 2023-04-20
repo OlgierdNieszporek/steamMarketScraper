c248859f3cd455c88ed87d12b7a23bb5970c89b5
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-20 15:07:28 +0200

    essa

--- project/pageScraper/multiThreading.py
+++ project/pageScraper/multiThreading.py
@@ -5,7 +5,8 @@ from project import app
 
 def master():
     while True:
-        app.run(host="localhost", port=8000, debug=True, use_reloader=False)
+        #app.run(host="localhost", port=8000, debug=True, use_reloader=False)
+        app.run(debug=True, use_reloader=False)
 
 def worker():
     while True:
