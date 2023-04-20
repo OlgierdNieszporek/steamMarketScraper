2ad3eac9f86b5d1014a981f93d71e9c7c6b9ae42
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-20 19:22:46 +0200

    proba 2010230123feaeffeefsaef

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,14 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from project.pageScraper.scheduler import *
+import multiprocessing
 
-runScheduler()
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(job, 'interval', seconds=10)
+sched.start()
+
+if not multiprocessing.current_process().name == 'MainProcess':
+    sched.shutdown()
 app = Flask("project")
 
 from project.controllers import *
