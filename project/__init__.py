4ae9c07acb5b6489c0f4bd602334f9c01f8ba139
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-22 22:01:20 +0200

    proba 2010230123feaeffeefsaef

--- project/__init__.py
+++ project/__init__.py
@@ -2,10 +2,11 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from project.pageScraper.scheduler import *
 
-
+print('chuj')
 app = Flask(__name__)
-
+print('dupa')
 
 from project.controllers import *
 
 runScheduler()
+print('kamieni kupa')
