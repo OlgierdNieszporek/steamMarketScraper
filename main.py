42bbd02ab1b9e9f15645b627d1cc8ff8eca7aa19
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-22 20:03:15 +0200

    proba 2010230123feaeffeefsaef

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ from project.pageScraper.scheduler import *
 if __name__ == '__main__':
     scheduler.add_job(id='Scheduled job', func=schedulerJob, trigger='interval', seconds=5)
     scheduler.start()
-    app.run(host='0.0.0.0', debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', debug=False, use_reloader=False)
