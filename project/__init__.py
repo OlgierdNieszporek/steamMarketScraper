fb43ee2363f74e998fe56deb0c4e0131dba15312
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-22 22:14:34 +0200

    proba 2010230123feaeffeefsaef

--- project/__init__.py
+++ project/__init__.py
@@ -2,11 +2,29 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from project.pageScraper.scheduler import *
 
+def job():
+
+    #screapeData
+    #loadScrapeToDatabase()
+
+    #deleteFromDBIfOlderThanTwoWeeks
+    #removeDataFromDatabase()
+
+    #generateNewCharts
+
+    #updateFront
+    print("Done scheduled tasks")
+
 print('chuj')
 app = Flask(__name__)
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(job, 'interval', seconds=5)
+sched.start()
+
+
 print('dupa')
 
 from project.controllers import *
 
-runScheduler()
+#runScheduler()
 print('kamieni kupa')
--- project/pageScraper/scheduler.py
+++ project/pageScraper/scheduler.py
@@ -1,29 +1,29 @@
-import atexit
-
-import schedule
-import time
-
-from apscheduler.schedulers.background import BackgroundScheduler
-
-from project.Database.databaseOperations import *
-def job():
-
-    #screapeData
-    #loadScrapeToDatabase()
-
-    #deleteFromDBIfOlderThanTwoWeeks
-    #removeDataFromDatabase()
-
-    #generateNewCharts
-
-    #updateFront
-    print("Done scheduled tasks")
-
-def runScheduler():
-    sched = BackgroundScheduler(daemon=True)
-    sched.add_job(job, 'interval', seconds=5)
-    sched.start()
-    atexit.register(lambda: sched.shutdown())
-
-# def schedulerJob():
-#     print("Jebać disa skurwysyna")
+# import atexit
+#
+# import schedule
+# import time
+#
+# from apscheduler.schedulers.background import BackgroundScheduler
+#
+# from project.Database.databaseOperations import *
+# def job():
+#
+#     #screapeData
+#     #loadScrapeToDatabase()
+#
+#     #deleteFromDBIfOlderThanTwoWeeks
+#     #removeDataFromDatabase()
+#
+#     #generateNewCharts
+#
+#     #updateFront
+#     print("Done scheduled tasks")
+#
+# def runScheduler():
+#     sched = BackgroundScheduler(daemon=True)
+#     sched.add_job(job, 'interval', seconds=5)
+#     sched.start()
+#     atexit.register(lambda: sched.shutdown())
+#
+# # def schedulerJob():
+# #     print("Jebać disa skurwysyna")
