0c1c7dd2379bdeec0b7821becea381df18d8f273
Olgierd Nieszporek <olgierdnieszporek@gmail.com>
2023-04-26 09:25:12 +0200

    front na product page

--- project/controllers/ProductPageController.py
+++ project/controllers/ProductPageController.py
@@ -1,4 +1,6 @@
 import os
+from typing import re
+
 import pandas as pd
 from project import app
 from flask import render_template, redirect, url_for, request
@@ -16,10 +18,14 @@ def show_index(product_name):
         'Date': [],
         'Price': []})
 
-    #product = getProductByName(product_name)
+    # product = getProductByName(product_name)
     productHistory = getProductHistory(product_name)
-    #productHistory = [['3.5500', '3.5500', '3.5500', '3.5500', '3.5500', '3.3400', '3.5500'], ['2023-04-19', '2023-04-20', '2023-04-21', '2023-04-22', '2023-04-23', '2023-04-24', '2023-04-25']]
+    # productHistory = [['3.5500', '3.5500', '3.5500', '3.5500', '3.5500', '3.3400', '3.5500'], ['2023-04-19', '2023-04-20', '2023-04-21', '2023-04-22', '2023-04-23', '2023-04-24', '2023-04-25']]
 
+    k1 = float(productHistory[0][6])
+    k0 = float(productHistory[0][5])
+
+    roi = round(((k1 - k0) / k0), 2)
 
     for i in range(0, len(productHistory[1])):
         df.loc[i] = [(productHistory[1])[i], (productHistory[0])[i]]
@@ -27,16 +33,8 @@ def show_index(product_name):
     dfTransposed = df.set_index(['Date', 'Price']).T
     table = dfTransposed.to_html(index=False, escape=False)
 
-    #return render_template("ProductPage.html.j2", table = table, name = product_name, price = product.productPrice)
-    return render_template("ProductPage.html.j2", table = table)
-    # product = getProductByName(product_name)
-    # #full_filename = os.path.join(app.config['CHART_FOLDER'], 'Wykres_' + product_name + '.png')
-    # print(CHART_FOLDER)
-    # full_filename = os.path.join(app.config['CHART_FOLDER'], 'Wykres_' + 'basic' + '.png')
-    #
-    # return render_template("ProductPage.html.j2",
-    #                       # product_chart_image=full_filename,
-    #                        product_name=product.productName,
-    #                        product_price=product.productPrice,
-    #                        utc_dt=product.date
-    #                        )
+    return render_template("ProductPage.html.j2",
+                           product_name=product_name,
+                           product_price=productHistory[0][6],
+                           product_roi=roi,
+                           table=table)  # , price = product.productPrice)
